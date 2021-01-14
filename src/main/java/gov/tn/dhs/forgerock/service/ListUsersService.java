36ab8b18ceaea85e107095bafec464dc011506d6
TN340BN <tn340bn@tn.gov>
2021-01-14 17:32:48 -0500

    Added logging statements

--- src/main/java/gov/tn/dhs/forgerock/service/ListUsersService.java
+++ src/main/java/gov/tn/dhs/forgerock/service/ListUsersService.java
@@ -7,6 +7,7 @@ import gov.tn.dhs.forgerock.model.UserInfo;
 import gov.tn.dhs.forgerock.util.ForgeRockUtil;
 import org.apache.camel.Exchange;
 import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,8 @@ public class ListUsersService extends BaseService {
     }
 
     public void process(Exchange exchange) {
-        String urlOverHttps = appProperties.getBaseurl() + "user?_queryFilter=true";
+//        String urlOverHttps = appProperties.getBaseurl() + "user?_queryFilter=true";
+        String urlOverHttps = appProperties.getBaseurl() + "user?_queryId=query-all";
         try {
             HttpResponse response = ForgeRockUtil.doGet(urlOverHttps);
             int statusCode = response.getStatusLine().getStatusCode();
@@ -44,6 +46,7 @@ public class ListUsersService extends BaseService {
                 default: {
                     String reasonPhrase = response.getStatusLine().getReasonPhrase();
                     String code = Integer.toString(response.getStatusLine().getStatusCode());
+                    logger.info("response: {}", EntityUtils.toString(response.getEntity()));
                     setupError(code, reasonPhrase);
                     break;
                 }
--- src/main/java/gov/tn/dhs/forgerock/util/ForgeRockUtil.java
+++ src/main/java/gov/tn/dhs/forgerock/util/ForgeRockUtil.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.tn.dhs.forgerock.config.AppProperties;
+import gov.tn.dhs.forgerock.service.ListUsersService;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -19,6 +22,8 @@ public class ForgeRockUtil {
 
     public static final String OPENIDM_USERNAME_HEADER = "X-OpenIDM-Username";
     public static final String OPENIDM_PASSWORD_HEADER = "X-OpenIDM-Password";
+
+    private static final Logger logger = LoggerFactory.getLogger(ForgeRockUtil.class);
 
     private static AppProperties appProperties;
 
@@ -31,13 +36,19 @@ public class ForgeRockUtil {
         HttpGet getMethod = new HttpGet(url);
         getMethod.addHeader(OPENIDM_USERNAME_HEADER, appProperties.getOpenidmUsername());
         getMethod.addHeader(OPENIDM_PASSWORD_HEADER, appProperties.getOpenidmPassword());
+        logger.info("calling URL [{}]", url);
+        long t1 = System.currentTimeMillis();
         HttpResponse response = httpClient.execute(getMethod);
+        long t2 = System.currentTimeMillis();
+        logger.info("received response from call");
+        logger.info("call to URL [{}] took {} ms", url, Long.toString(t2-t1));
         return response;
     }
 
     public static JsonNode getJsonFromResponse(HttpResponse response) throws IOException {
         HttpEntity entity = response.getEntity();
         String jsonString = EntityUtils.toString(entity);
+        logger.info("JSON response received: {}", jsonString);
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(jsonString);
         return jsonNode;
